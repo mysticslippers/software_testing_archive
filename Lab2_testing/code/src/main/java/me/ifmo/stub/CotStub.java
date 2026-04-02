f75c4f1037b701f0daf902a0322f5b84cd932463
Mikhailov Dmitry <misticslippers@gmail.com>
2026-04-02 20:07:37 +0300

    add stubs

---
+++ Lab2_testing/code/src/main/java/me/ifmo/stub/CotStub.java
@@ -0,0 +1,10 @@
+package me.ifmo.stub;
+
+public class CotStub extends AbstractTableStub {
+
+    public CotStub() {
+        put(-Math.PI / 4, -1.0);
+        put(-Math.PI / 3, -1.0 / Math.sqrt(3));
+        put(-Math.PI / 6, -Math.sqrt(3));
+    }
+}
---
+++ Lab2_testing/code/src/main/java/me/ifmo/stub/CscStub.java
@@ -0,0 +1,10 @@
+package me.ifmo.stub;
+
+public class CscStub extends AbstractTableStub {
+
+    public CscStub() {
+        put(-Math.PI / 4, -Math.sqrt(2));
+        put(-Math.PI / 3, -2.0 / Math.sqrt(3));
+        put(-Math.PI / 6, -2.0);
+    }
+}
